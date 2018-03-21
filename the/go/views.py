c7a41ae648b3764b1f8b204f53de5e7a2d7e1735
vincentmuya <vincentmuya13@gmail.com>
2018-03-21 09:05:02 +0300

    defined app in settings and configured the project urls and created view function for index and landing page

---
+++ the/go/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.db import transaction
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")

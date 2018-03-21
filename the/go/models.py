29cf93eaf184fc4c4c837e135a94f0d07a49e547
vincentmuya <vincentmuya13@gmail.com>
2018-03-21 11:25:27 +0300

    created location class and installed google maps

--- the/go/models.py
+++ the/go/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharFIeld(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
